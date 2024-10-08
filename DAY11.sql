815ab59439426212c4d67d84545f1a7aea768bd3
santhu25 <107323039+santhu25@users.noreply.github.com>
2024-10-08 23:48:22 +0530

    Update DAY11.sql

--- DAY11.sql
+++ DAY11.sql
@@ -37,8 +37,22 @@ values
 insert into Shippers(companyName)
 values
 ('fedEx'),('Amazon'),('DHL');
-
-insert into Products(supplierId, category, productId, productName, quantityPerUnit,  unitsInStock, unitsInOrder,reOrderLevel, Dis)
+insert into Products(supplierid,
+category, 
+productName,
+quantityPerUnit,  
+unitsInStock,
+unitsInOrder,
+reOrderLevel,
+Discontinued)
 values
 (1,2,'laptop','1 unit', 50,10,5,FALSE),
 (2,1,'samart phone','1 unit',100,20,10, FALSE);
+
+insert into orders(customerId,empId,orderDate,requiredDate,shippedDate,shipvia,shipName,shipAddress,shipCity,shipRegion,shipCountry)
+values
+(1,101,'2024-10-01','2024-10-05','2024-10-03',1,'Acme corp','123 Main street','New York','NY','USA');
+insert into orderDetails(orderId,productId,unitPrice,quantity,discount)
+values
+(1,1,1000.00,2,0.05),
+(2,2,600.00,3,0.10);
